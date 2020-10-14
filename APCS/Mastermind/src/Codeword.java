970f49eac31b3463ceb9be57619aa3d13ce5a126
Mike Magruder <mikemag@outlook.com>
2020-10-14 00:02:52 -0700

    Allow different color and pin counts
    The standard game is 4 pins of 6 colors each. Generalized the program to be able to play with 1-9 colors and 1-9 pins. A little misc cleanup along the way, but otherwise this is largely unchanged. Still defaults to a 6/4 game.

--- APCS/Mastermind/src/Codeword.java
+++ APCS/Mastermind/src/Codeword.java
@@ -3,70 +3,62 @@
 // This source code is licensed under the MIT license found in the
 // LICENSE file in the root directory of this source tree.
 
+import java.util.Arrays;
+
 // Class to hold a codeword for the Mastermind game.
-// 
-// This is represented two ways just for convenience: one as an array of individual 
-// digits, and one as a simple integer.
+//
+// This is represented two ways just for convenience: one as an array of individual digits, and one
+// as a simple integer.
 
 public class Codeword {
 
-  byte[] digits = new byte[4];
-  int cacheKey;
+  private final byte[] digits;
+  private int cacheKey;
+  public static final int winningScore = Mastermind.pinCount * 10; // "40" for a 4-pin game.
 
-  // This is an optimization for codeword comparison that trades space for time.
-  // It is a sparse array large enough to hold the combination of two 4-digit
-  // codewords up to 9 colors each.
+  // This is an optimization for codeword comparison that trades space for time. It is a sparse 2D
+  // array large enough to hold a byte for every paid of codewords. This allows us to lazily compute
+  // all codeword combinations and find them with a simple array lookup, which is far, far faster
+  // than actually comparing two codewords.
   //
-  // Given c1 and c2, the key to the array is c1c2.
-  //
-  // This allows us to lazily compute all codeword combinations and find them
-  // with a simple array lookup, which is far, far faster than actually comparing
-  // two codewords.
-  private static final byte[] checkMap = new byte[99999999];
+  // However, this simple scheme falls apart for codewords above 5 pins, as the memory requirements
+  // exceed what's reasonable. So we only use it for small pin games.
+  private static final byte[][] checkMap;
 
   static {
-    for (int i = 0; i < checkMap.length; i++) {
-      checkMap[i] = -1;
+    if (Mastermind.pinCount <= 5) {
+      int size = (int) Math.pow(10, Mastermind.pinCount);
+      checkMap = new byte[size][size];
+      for (byte[] bytes : checkMap) {
+        Arrays.fill(bytes, (byte) -1);
+      }
     }
   }
 
-  // Helper class to count how many times we call check() in some of our
-  // algorithms.
-  public static class CheckCounter {
-
-    private long n = 0;
-
-    public void recordCheck() {
-      n++;
+  Codeword(byte[] digits) {
+    this.digits = digits;
+    for (byte digit : digits) {
+      this.cacheKey = this.cacheKey * 10 + digit;
     }
-
-    public long getCount() {
-      return n;
-    }
-  }
-
-  Codeword(int a, int b, int c, int d) {
-    this.digits[0] = (byte) a;
-    this.digits[1] = (byte) b;
-    this.digits[2] = (byte) c;
-    this.digits[3] = (byte) d;
-    cacheKey = a * 1000 + b * 100 + c * 10 + d;
   }
 
   // The result of this is encoded in a single byte for the cache.
   public int check(Codeword guess) {
-    byte cached = checkMap[cacheKey * 10000 + guess.cacheKey];
-    if (cached < 0) {
+    byte result = -1;
+    if (checkMap != null) {
+      result = checkMap[cacheKey][guess.cacheKey];
+    }
+    if (result < 0) {
       int b = 0;
       int w = 0;
-      boolean[] used = new boolean[4];
+      boolean[] used = new boolean[Mastermind.pinCount];
 
-      for (int i = 0; i < 4; i++) {
+      for (int i = 0; i < Mastermind.pinCount; i++) {
         if (guess.digits[i] == digits[i]) {
           b++;
           used[i] = true;
         } else {
-          for (int j = 0; j < 4; j++) {
+          for (int j = 0; j < Mastermind.pinCount; j++) {
             if (!used[j] && guess.digits[i] == digits[j] && guess.digits[j] != digits[j]) {
               w++;
               used[j] = true;
@@ -75,20 +67,13 @@ public class Codeword {
           }
         }
       }
-      cached = (byte) (b * 10 + w);
-      checkMap[cacheKey * 10000 + guess.cacheKey] = cached;
+      result = (byte) (b * 10 + w);
+      if (checkMap != null) {
+        checkMap[cacheKey][guess.cacheKey] = result;
+      }
     }
-    return cached;
-  }
 
-  public boolean equals(Object o) {
-    if (o instanceof Codeword) {
-      Codeword x = (Codeword) o;
-      return digits[0] == x.digits[0] && digits[1] == x.digits[1] && digits[2] == x.digits[2]
-          && digits[3] == x.digits[3];
-
-    }
-    return false;
+    return result;
   }
 
   public String toString() {
