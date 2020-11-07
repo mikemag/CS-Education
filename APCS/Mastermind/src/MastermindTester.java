d6b678a91fd10e6193836de76718cc0be3a68dc2
Mike Magruder <mikemag@outlook.com>
2020-11-07 01:40:36 -0800

    Mastermind test generator and runner
    Cleaned up the test case generator.
    Setup a runner to use on student scoring functions.

--- APCS/Mastermind/src/MastermindTester.java
+++ APCS/Mastermind/src/MastermindTester.java
@@ -1,69 +1,92 @@
-import java.io.FileWriter;
-import java.util.ArrayList;
+import java.io.FileReader;
+import java.util.Scanner;
 
 public class MastermindTester {
 
-  // Make a list of all codewords for a given number of "colors". Colors are represented by the
-  // digits 0 thru n. This figures out how many codewords there are, which is colorCount ^ pinCount,
-  // then converts the base-10 number of each codeword to it's base-colorCount representation.
-  private static ArrayList<byte[]> makeAllCodewords(int colorCount, int pinCount) {
-    int totalWords = (int) Math.pow(colorCount, pinCount);
-    ArrayList<byte[]> l = new ArrayList<>(totalWords);
+  private static final String testFilename = "mastermind_4p6c.txt";
 
-    for (int i = 0; i < totalWords; i++) {
-      int w = i;
-      byte[] digits = new byte[pinCount];
-      int di = pinCount - 1;
-      do {
-        digits[di--] = (byte) (w % colorCount);
-        w /= colorCount;
-      } while (w > 0);
-
-      // Colors start at 1, not 0.
-      for (di = 0; di < pinCount; di++) {
-        digits[di] += 1;
-      }
-
-      l.add(digits);
+  private static byte[] codeStringToBytes(String s) {
+    byte[] r = s.getBytes();
+    for (int i = 0; i < r.length; i++) {
+      r[i] -= '0';
     }
-
-    return l;
+    return r;
   }
 
-  public static void writeAllScores(ArrayList<byte[]> allCodewords)
-  {
+  private static int resultsToInt(String b, String w) {
+    return Integer.parseInt(b) * 10 + Integer.parseInt(w);
+  }
+
+  private static void runTestsFromFile(String filename) {
+    System.out.println("Running tests from file " + filename);
     try {
-      FileWriter fw = new FileWriter("mastermind_4x6.txt");
-      fw.write("Secret,Guess,Correct Digit and Location,Correct Digit Wrong Location\n");
-      for (byte[] a : allCodewords) {
-        Codeword ca = new Codeword(a);
-        for (byte[] b : allCodewords) {
-          Codeword cb = new Codeword(b);
-          int r1 = ca.score(cb);
-          fw.write(String.format("%s,%s,%d,%d\n", ca, cb, r1 / 10, r1 % 10));
+      int total = 0;
+      FileReader fr = new FileReader(filename);
+      Scanner lineScanner = new Scanner(fr);
+      lineScanner.nextLine(); // Drop the header row
+
+      Results mikemag = new Results("mikemag"); // Use your own name for your results
+      while (lineScanner.hasNextLine()) {
+        total++;
+        String line = lineScanner.nextLine();
+        String[] testData = line.split(",");
+
+        // Update this block to work for your scoring function. Convert testData as needed.
+        Codeword ca = new Codeword(codeStringToBytes(testData[0]));
+        Codeword cb = new Codeword(codeStringToBytes(testData[1]));
+        int expected = resultsToInt(testData[2], testData[3]);
+        int r = ca.score(cb);
+        if (r == expected) {
+          mikemag.success();
+        } else {
+          mikemag.failure(testData, String.format("%02d", r));
         }
       }
-      fw.close();
+
+      System.out.format("Done running %,d test cases.\n\n", total);
+      System.out.println(mikemag);
     } catch (Exception e) {
       System.out.println(e);
     }
   }
 
   public static void main(String[] args) {
-    ArrayList<byte[]> allCodewords = makeAllCodewords(6, 4);
-//    writeAllScores(allCodewords);
+    runTestsFromFile(testFilename);
+  }
 
-    for (byte[] a : allCodewords) {
-      Codeword ca = new Codeword(a);
-      for (byte[] b : allCodewords) {
-        Codeword cb = new Codeword(b);
-        int r1 = ca.score(cb);
-        int r2 = ca.scoreOriginal(cb);
-        if (r1 != r2) {
-          System.out.printf("Wrong answer: %s %s %d %d\n", ca, cb, r1, r2);
-        }
+  // Helper class to hold the results of a test run.
+  static class Results {
+
+    private final String name;
+    private int totalRun;
+    private int totalFailed;
+    private String firstFailure;
+
+    public Results(String name) {
+      this.name = name;
+    }
+
+    public void success() {
+      totalRun++;
+    }
+
+    public void failure(String[] testData, String result) {
+      totalRun++;
+      totalFailed++;
+      if (firstFailure == null) {
+        firstFailure = String.format("%s vs %s expected %s%s, got %s",
+            testData[0], testData[1], testData[2], testData[3], result);
       }
     }
 
+    @Override
+    public String toString() {
+      String r = String.format("%s: passed %.2f%%",
+          name, (float) (totalRun - totalFailed) / totalRun * 100.0);
+      if (firstFailure != null) {
+        r += ", first failure: " + firstFailure;
+      }
+      return r;
+    }
   }
 }
---
+++ APCS/Mastermind/src/TestGenerator.java
@@ -0,0 +1,81 @@
+import java.io.FileWriter;
+
+public class TestGenerator {
+
+  private static final boolean includeMiyoshiTests = true;
+
+  // Generate a codeword from an int given a number of pins and colors. Pin values start at 1.
+  // Converts the given base-10 number to it's base-colorCount representation.
+  private static byte[] generateCodeword(int w, int pinCount, int colorCount) {
+    byte[] digits = new byte[pinCount];
+    int di = pinCount - 1;
+    do {
+      digits[di--] = (byte) (w % colorCount);
+      w /= colorCount;
+    } while (w > 0);
+
+    // Colors start at 1, not 0.
+    for (di = 0; di < pinCount; di++) {
+      digits[di] += 1;
+    }
+    return digits;
+  }
+
+  private static final String[] miyoshiTests = {
+      "6684,0000,0,0",
+      "6684,6666,2,0",
+      "6684,0123,0,0",
+      "6684,4567,0,2",
+      "6684,4589,1,1",
+      "6684,6700,1,0",
+      "6684,0798,0,1",
+      "6684,6484,3,0",
+      "6684,6480,2,1",
+      "6684,6884,3,0",
+      "6684,6684,4,0",
+      "6684,8468,0,3",
+      "6684,8866,0,3",
+      "6684,8466,0,4",
+  };
+
+  private static void writeAllScores(int pinCount, int colorCount) {
+    try {
+      int total = 0;
+      String filename = String.format("mastermind_%dp%dc.txt", pinCount, colorCount);
+      System.out.println("Writing tests to " + filename);
+      FileWriter fw = new FileWriter(filename);
+      fw.write("Secret,Guess,Correct Digit and Location,Correct Digit Wrong Location\n");
+
+      if (includeMiyoshiTests) {
+        System.out.println("Including Miyoshi's test cases");
+        for (String t : miyoshiTests) {
+          fw.write(t);
+          fw.write("\n");
+          total++;
+        }
+      }
+
+      int totalWords = (int) Math.pow(colorCount, pinCount);
+      for (int i = 0; i < totalWords; i++) {
+        Codeword ca = new Codeword(generateCodeword(i, pinCount, colorCount));
+        for (int j = 0; j < totalWords; j++) {
+          Codeword cb = new Codeword(generateCodeword(j, pinCount, colorCount));
+          int r1 = ca.score(cb);
+          fw.write(String.format("%s,%s,%d,%d\n", ca, cb, r1 / 10, r1 % 10));
+          total++;
+        }
+      }
+
+      fw.close();
+      System.out.format("Wrote %,d test cases\n", total);
+    } catch (Exception e) {
+      System.out.println(e);
+    }
+  }
+
+  public static void main(String[] args) {
+    int pinCount = 4;
+    int colorCount = 6;
+    writeAllScores(pinCount, colorCount);
+  }
+}
