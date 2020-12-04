05f72b8729b4851b692bf515036afcb27c332f4d
Mike Magruder <mikemag@outlook.com>
2020-12-04 12:00:08 -0800

    Simplify a bit

--- Intro_CXX/StringExamples/main.cpp
+++ Intro_CXX/StringExamples/main.cpp
@@ -22,11 +22,9 @@ using namespace std;
 
 int main() {
   // Build up a string a bit at a time with + and +=.
-  {
-    string s = "AB";
-    s = "CD" + s + "EF";
-    cout << s << endl;  // CDABEF
-  }
+  string s = "AB";
+  s = "CD" + s + "EF";
+  cout << s << endl;  // CDABEF
 
 
   // Build up a string a bit at a time with <<. Be sure to #include <sstream>
@@ -37,160 +35,128 @@ int main() {
 
 
   // Convert a string to all lower case, requires #include <algorithm>
-  {
-    string s = "ABCDEFG";
-    cout << "[" << s << "]\n";
-    transform(begin(s), end(s), begin(s), ::tolower);
-    cout << "[" << s << "]\n";  // [abcdefg]
-  }
+  s = "AbCdEfG";
+  cout << "[" << s << "]\n";
+  transform(begin(s), end(s), begin(s), ::tolower);
+  cout << "[" << s << "]\n";  // [abcdefg]
 
 
   // Is there a specific character anywhere in a string?
   // http://www.cplusplus.com/reference/string/string/find/
-  {
-    string s = "Hi there! Have fun.";
-    cout << s << endl;
-    if (s.find('!') != string::npos) {
-      cout << "String has a '!' in it!" << endl;
-    }
+  s = "Hi there! Have fun.";
+  cout << s << endl;
+  if (s.find('!') != string::npos) {
+    cout << "String has a '!' in it!" << endl;
   }
 
 
   // Convert a number to a string
   // http://www.cplusplus.com/reference/string/to_string/
-  {
-    string s = to_string(4.2);
-    cout << "[" << s << "]\n";  // [4.200000]
-  }
+  s = to_string(4.2);
+  cout << "[" << s << "]\n";  // [4.200000]
 
 
   // Does a string start with a number (and so might work with stoi() or stof())?
   // http://www.cplusplus.com/reference/cctype/isdigit/
-  {
-    string s = "42.2";
-    cout << "[" << s << "]\n";
-    if (isdigit(s[0])) {
-      float f = stof(s);
-      cout << "Float: " << f << endl;  // Float: 42.2
-    }
+  s = "42.2";
+  cout << "[" << s << "]\n";
+  if (isdigit(s[0])) {
+    float f = stof(s);
+    cout << "Float: " << f << endl;  // Float: 42.2
   }
 
 
   // Find a string within a string.
-  {
-    string s = "Hi there! Have fun.";
-    cout << s << endl;
-    if (s.find("ere") != string::npos) {
-      cout << "String has 'ere' in it!" << endl;
-    }
+  s = "Hi there! Have fun.";
+  cout << s << endl;
+  if (s.find("ere") != string::npos) {
+    cout << "String has 'ere' in it!" << endl;
   }
 
 
   // Take the first 2 characters of a string.
   // http://www.cplusplus.com/reference/string/string/substr/
-  {
-    string s = "ABCDEFG";
-    cout << "[" << s << "]\n";
-    s = s.substr(0, 2);
-    cout << "[" << s << "]\n";  // [AB]
-  }
+  s = "ABCDEFG";
+  cout << "[" << s << "]\n";
+  s = s.substr(0, 2);
+  cout << "[" << s << "]\n";  // [AB]
 
 
   // Take the middle n of a string, in the case the middle 3, skipping 2.
-  {
-    string s = "ABCDEFG";
-    cout << "[" << s << "]\n";
-    s = s.substr(2, 3);
-    cout << "[" << s << "]\n";  // [CDE]
-  }
+  s = "ABCDEFG";
+  cout << "[" << s << "]\n";
+  s = s.substr(2, 3);
+  cout << "[" << s << "]\n";  // [CDE]
 
 
   // Take the last n of a string, the last 2 in the case.
-  {
-    string s = "ABCDEFG";
-    cout << "[" << s << "]\n";
-    s = s.substr(s.length() - 2);
-    cout << "[" << s << "]\n";  // [FG]
-  }
+  s = "ABCDEFG";
+  cout << "[" << s << "]\n";
+  s = s.substr(s.length() - 2);
+  cout << "[" << s << "]\n";  // [FG]
 
 
   // Get first character, last character.
   // http://www.cplusplus.com/reference/string/string/front/
   // http://www.cplusplus.com/reference/string/string/back/
-  {
-    string s = "abcd";
-    cout << "First: [" << s.front() << "]\n";  // First: [a]
-    cout << "Last: [" << s.back() << "]\n";  // Last: [d]
-  }
+  s = "abcd";
+  cout << "First: [" << s.front() << "]\n";  // First: [a]
+  cout << "Last: [" << s.back() << "]\n";  // Last: [d]
 
 
   // Insert into a string.
   // http://www.cplusplus.com/reference/string/string/insert/
-  {
-    string s = "ABCDEFG";
-    cout << "[" << s << "]\n";
-    s.insert(2, "***");
-    cout << "[" << s << "]\n";  // [AB***CDEFG]
-  }
+  s = "ABCDEFG";
+  cout << "[" << s << "]\n";
+  s.insert(2, "***");
+  cout << "[" << s << "]\n";  // [AB***CDEFG]
 
 
   // Replace part of a string.
   // http://www.cplusplus.com/reference/string/string/replace/
-  {
-    string s = "ABCDEFG";
-    cout << "[" << s << "]\n";
-    s.replace(3, 2, "***");  // Replace 2 characters starting at index 3 with ***.
-    cout << "[" << s << "]\n";  // [ABC***FG]
-  }
+  s = "ABCDEFG";
+  cout << "[" << s << "]\n";
+  s.replace(3, 2, "***");  // Replace 2 characters starting at index 3 with ***.
+  cout << "[" << s << "]\n";  // [ABC***FG]
 
 
   // Trim spaces off the end of a string, i.e., rtrim.
   // http://www.cplusplus.com/reference/string/string/erase/
   // http://www.cplusplus.com/reference/string/string/find_last_not_of/
   // http://www.cplusplus.com/reference/string/string/find_first_not_of/
-  {
-    string s = "Hello    ";
-    cout << "[" << s << "]\n";
-    s.erase(s.find_last_not_of(' ') + 1);
-    cout << "[" << s << "]\n";  // [Hello]
-  }
+  s = "Hello    ";
+  cout << "[" << s << "]\n";
+  s.erase(s.find_last_not_of(' ') + 1);
+  cout << "[" << s << "]\n";  // [Hello]
 
 
   // Trim spaces off the front of a string, i.e., ltrim.
-  {
-    string s = "    Hello";
-    cout << "[" << s << "]\n";
-    s.erase(0, s.find_first_not_of(' '));
-    cout << "[" << s << "]\n";  // [Hello]
-  }
+  s = "    Hello";
+  cout << "[" << s << "]\n";
+  s.erase(0, s.find_first_not_of(' '));
+  cout << "[" << s << "]\n";  // [Hello]
 
 
   // Trim spaces off the front and back of a string, i.e., trim.
-  {
-    string s = "    Hello   ";
-    cout << "[" << s << "]\n";
-    s.erase(0, s.find_first_not_of(' '));
-    s.erase(s.find_last_not_of(' ') + 1);
-    cout << "[" << s << "]\n";  // [Hello]
-  }
+  s = "    Hello   ";
+  cout << "[" << s << "]\n";
+  s.erase(0, s.find_first_not_of(' '));
+  s.erase(s.find_last_not_of(' ') + 1);
+  cout << "[" << s << "]\n";  // [Hello]
 
 
   // Pad out a string with more spaces. This will clip a long string, or extend a short one.
-  {
-    string s = "Hi";
-    cout << "[" << s << "]\n";
-    s.resize(10, ' ');
-    cout << "[" << s << "]\n";  // [Hi        ]
-  }
+  s = "Hi";
+  cout << "[" << s << "]\n";
+  s.resize(10, ' ');
+  cout << "[" << s << "]\n";  // [Hi        ]
 
 
   // Pad out a string of a float with more spaces or 0s.
-  {
-    string s = "42.123";
-    cout << "[" << s << "]\n";
-    s.resize(s.find('.') + 1 + 6, '0');  // Ensure 6 positions beyond the decimal point.
-    cout << "[" << s << "]\n";  // [42.123000]
-  }
+  s = "42.123";
+  cout << "[" << s << "]\n";
+  s.resize(s.find('.') + 1 + 6, '0');  // Ensure 6 positions beyond the decimal point.
+  cout << "[" << s << "]\n";  // [42.123000]
 
   return 0;
 }
